e81d5faddcf4ca99e1e72f9d3135c82ea226b76d
Aurelio Marcus <68666645+AurelioMarcusTO@users.noreply.github.com>
2021-06-02 12:14:38 -0300

    Estudo do modulo Calendar e criacao da segunda classe para estudo

--- modulo_datas/src/modulo_datas/DatasEmJava.java
+++ modulo_datas/src/modulo_datas/DatasEmJava.java
@@ -1,10 +1,34 @@
 package modulo_datas;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
+
 public class DatasEmJava {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws ParseException {
 		
-		
+	SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd/MM/yyyy");
+	
+	Date dataVencimentoBoleto = simpleDateFormat.parse("10/04/2021");
+	
+	Date dataAtualHoje = simpleDateFormat.parse("07/04/2021");
+	
+	/*A data do vencimento é posterior a data atual*/
+	if (dataVencimentoBoleto.after(dataAtualHoje)) {
+		System.out.println("O boleto não esta vencido ! ");
+	}else {
+		System.out.println("O boleto esta vencido !");
+	}
+	
+	/*A data Atual é depois da data do vencimento*/
+	if (dataAtualHoje.before(dataVencimentoBoleto)) {
+		System.out.println("O boleto não esta vencido ! ");
+	}else {
+		System.out.println("O boleto esta vencido !");
+	}
 
 	}
 
---
+++ modulo_datas/src/modulo_datas/DatasEmJava2.java
@@ -0,0 +1,24 @@
+package modulo_datas;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
+public class DatasEmJava2 {
+
+	public static void main(String[] args) throws ParseException {
+		
+		Calendar calendar = Calendar.getInstance(); /*Pega a data atual*/ 
+		
+		/*Simular que data vem do banco de dados*/
+		
+		calendar.setTime(new SimpleDateFormat("dd-MM-yyyy").parse("10-03-2021")); /*Definindo uma data qualquer*/ 
+		
+		
+		calendar.add(Calendar.DAY_OF_MONTH, 5);
+		
+		
+
+	}
+
+}
